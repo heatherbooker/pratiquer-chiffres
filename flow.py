075447a6f18b4af8351a1a8abf8808a65b3321aa
hboo <heather.booker@hotmail.com>
2020-10-06 21:33:35 -0400

    Organize happy little code into functions

--- flow.py
+++ flow.py
@@ -3,21 +3,38 @@ import sys
 import requests
 import base64
 
-auth = subprocess.run(['gcloud', 'auth', 'application-default', 'print-access-token'], capture_output=True, text=True).stdout.strip()
-headers = {
-    'Authorization': f'Bearer {auth}',
-    'Content-Type': 'application/json; charset=utf-8',
-}
-url = 'https://texttospeech.googleapis.com/v1/text:synthesize'
-req_data = open('request.json')
+API_URL = 'https://texttospeech.googleapis.com/v1/text:synthesize'
 
-response = requests.post(url, headers=headers, data=req_data)
-print(response.json())
+def get_auth():
+    command = ['gcloud', 'auth', 'application-default', 'print-access-token']
+    command_output = subprocess.run(
+            command,
+            capture_output=True,
+            text=True)
+    return command_output.stdout.strip()
 
-encoded_audio = response.json()['audioContent']
+def send_request(data, auth):
+    headers = {
+        'Authorization': f'Bearer {auth}',
+        'Content-Type': 'application/json; charset=utf-8',
+    }
 
-audio = base64.b64decode(encoded_audio)
+    return requests.post(API_URL, headers=headers, data=data)
 
-outfile = open('result.mp3', 'wb')
-outfile.write(audio)
-outfile.close()
+def process_response(response):
+    encoded_audio = response.json()['audioContent']
+    return base64.b64decode(encoded_audio)
+
+def write_file(bytes):
+    outfile = open('result.mp3', 'wb')
+    outfile.write(bytes)
+    outfile.close()
+
+def main():
+    req_data = open('request.json')
+    auth = get_auth()
+    api_response = send_request(req_data, auth)
+    audio = process_response(api_response)
+    write_file(audio)
+
+main()
